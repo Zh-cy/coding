26995e970e70a9832345996e75c9e59a9a9da04f
Zh-cy <chunyang26604@gmail.com>
2021-06-21 14:21:42 +0200

    add offer19

---
+++ cpp/leetcode/offer19/match.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool isMatch(string s, string p) {
+    int m = s.size();
+    int n = p.size();
+    vector<vector<bool>> dp(m+1, vector<bool>(n+1, false));
+    dp[0][0] = true;
+    for (int i=2; i<n+1; i+=2)
+        dp[0][i] = dp[0][i-2] && p[i-1] == '*';
+
+    for(int i=1; i<m+1; i++)
+    {
+        for (int j=1; j<n+1; j++)
+        {
+            if (p[j-1] != '*')
+                dp[i][j] =  dp[i-1][j-1] && (s[i-1]==p[j-1] || p[j-1] == '.');
+            else
+                dp[i][j] = (dp[i][j - 2]) || (dp[i - 1][j] && (s[i - 1] == p[j - 2] || p[j - 2] == '.'));
+        }
+    }
+    return dp[m][n];
+}
+
+
+int main(void)
+{
+    string s = "aaaa";
+    string p = "a*a";
+    bool match = isMatch(s, p);
+    return 0;
+}
